7c8036ba11d248a103c70f8af373506593c5719e
sathvikakotra <sathvikakotra@gmail.com>
2025-08-07 18:20:00 +0530

    data

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,12 +3,13 @@ import Profileinfo from "../Cards/Profileinfo";
 import { useNavigate } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
 
-const Navbar = () => {
+const Navbar = ({ userInfo }) => {
   const [searchQuery, setSearchQuery] = useState("");
 
-  const navigate = useNavigate;
+  const navigate = useNavigate();
 
   const onLogout = () => {
+    localStorage.clear();
     navigate("/login");
   };
   const handleSearch = () => {};
@@ -30,7 +31,7 @@ const Navbar = () => {
         onClearSearch={onClearSearch}
       />
 
-      <Profileinfo onLogout={onLogout} />
+      <Profileinfo userInfo={userInfo} onLogout={onLogout} />
     </div>
   );
 };
--- frontend/src/pages/SignUp/SignUp.jsx
+++ frontend/src/pages/SignUp/SignUp.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
+import axiosInstance from "../../utils/axiosInstance";
 
 const PasswordInput = ({ value, onChange }) => {
   return (
@@ -13,12 +14,17 @@ const PasswordInput = ({ value, onChange }) => {
     />
   );
 };
-
+const validateEmail = (email) => {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+};
 const SignUp = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
 
   const handleSignUp = async (e) => {
     e.preventDefault();
@@ -39,6 +45,33 @@ const SignUp = () => {
     setError("");
 
     //SignUp api
+    try {
+      const response = await axiosInstance.post("/create-account", {
+        fullName: name,
+        email,
+        password,
+      });
+      // Handle successful registeration response
+      if (response.data && response.data.error) {
+        setError(response.data.message);
+        return;
+      }
+      if (response.data && response.data.accessToken) {
+        localStorage.setItem("token", response.data.accessToken);
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      // Handle login error
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        setError(error.response.data.message);
+      } else {
+        setError("An unexpected error occurred. Please try again.");
+      }
+    }
   };
 
   return (
